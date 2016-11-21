b4953a52718dd3e2070400ddf580308c9c72df93
Lukas Elmer <lukas.elmer@renuo.ch>
2016-11-21 18:55:03 +0100

    use puma

---
+++ config/puma.rb
@@ -0,0 +1,10 @@
+workers Integer(2)
+threads_count = Integer(2)
+threads threads_count, threads_count
+
+preload_app!
+
+rackup      DefaultRackup
+port        ENV['PORT']     || 3000
+environment ENV['RACK_ENV'] || 'development'
+
