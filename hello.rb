a614440ff5797e33a5a781cc59d8304c1cdab697
Lukas Elmer <lukas.elmer@renuo.ch>
2016-11-21 19:33:34 +0100

    remove sleep method

--- hello.rb
+++ hello.rb
@@ -4,13 +4,9 @@ get '/' do
   "<h1>Hello Swiss Engineering</h1><p>It is #{Time.now}</p>"
 end
 
-get '/sleep' do
-  sleep(0.2)
-  "<h1>Good morning</h1><p>I just slept for 0.5 seconds.</p>"
-end
-
 get '/simulate_work' do
   x=1
   50_000.times { x *= 2 }
   "2^50_000 is #{x}"
 end
+
