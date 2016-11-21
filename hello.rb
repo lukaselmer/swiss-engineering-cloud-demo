624c6cbac1665dd6e76a2304907f4c8e424d4c59
Lukas Elmer <lukas.elmer@renuo.ch>
2016-11-21 19:39:36 +0100

    add some more examples

--- hello.rb
+++ hello.rb
@@ -1,12 +1,19 @@
 require 'sinatra'
 
 get '/' do
-  "<h1>Hello Swiss Engineering</h1><p>It is #{Time.now}</p>"
+  power = rand(20_000) + 40_000
+  [
+    '<h1>Hello Swiss Engineering</h1>',
+    "<p>It is #{Time.now}</p>",
+    "<p><a href='/simulate_work?power=#{power}'>Calculate 2 ^ #{power}</a>",
+    "<p><a href='/'>Different number</a>"
+  ].join
 end
 
 get '/simulate_work' do
-  x=1
-  50_000.times { x *= 2 }
-  "2^50_000 is #{x}"
+  x = 1
+  power = params['power'].to_i
+  power.times { x = x * 2 }
+  "2^#{power} is #{x}"
 end
 
